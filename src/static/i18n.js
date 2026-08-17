7122cd49915819c6cb4cb4fee3bb6cca25918c81
inetconnector <frede@inetconnector.com>
2026-08-18 00:12:39 +0200

    fix: load approval layout repair

--- src/static/i18n.js
+++ src/static/i18n.js
@@ -3,5 +3,5 @@
   'use strict';
   // Keep the large localization dictionary isolated and parser-blocking so the
   // existing inline application script can use LocalCodeI18n immediately.
-  document.write('<script src="/i18n_base.js"><\/script><script src="/ui_polish.js"><\/script>');
+  document.write('<script src="/i18n_base.js"><\/script><script src="/ui_polish.js"><\/script><script src="/ui_approval_fix.js"><\/script>');
 })();
